3c35c99ebdf7368733d43ac1f200773a08523a69
zwwuu <98434813+zwwuu@users.noreply.github.com>
2023-06-25 20:04:38 -0400

    style: update slider style

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -2,23 +2,21 @@ import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
 import * as SliderPrimitive from "@radix-ui/react-slider";
 import clsx from "clsx";
 
-const Slider = forwardRef<
-  ElementRef<typeof SliderPrimitive.Root>,
-  ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
+type SliderProps = ComponentPropsWithoutRef<typeof SliderPrimitive.Root>;
+const Slider = forwardRef<ElementRef<typeof SliderPrimitive.Root>, SliderProps>(({ className, ...props }, ref) => (
   <SliderPrimitive.Root
     className={clsx("relative flex w-full touch-none select-none items-center", className)}
     ref={ref}
     {...props}
   >
-    <SliderPrimitive.Track className={"relative h-2 w-full grow overflow-hidden bg-primary-200"}>
-      <SliderPrimitive.Range className={"absolute h-full bg-primary-600"} />
+    <SliderPrimitive.Track className={"relative h-2 w-full grow overflow-hidden border bg-base-300"}>
+      <SliderPrimitive.Range className={"absolute h-full bg-primary"} />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb
       className={
-        "block h-6 w-6 border bg-primary focus:outline-dashed focus:outline-2 focus:outline-offset-2 focus:outline-current"
+        "block h-5 w-5 border bg-primary focus:outline-dashed focus:outline-2 focus:outline-offset-2 focus:outline-current"
       }
-    />
+    ></SliderPrimitive.Thumb>
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
