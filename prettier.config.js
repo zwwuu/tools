d40995c4a57bd2f63f40d3145e742e6fccb4e23a
zwwuu <98434813+zwwuu@users.noreply.github.com>
2023-06-28 18:55:42 -0400

    chore: update dependencies

--- prettier.config.js
+++ prettier.config.js
@@ -20,5 +20,5 @@ module.exports = {
     "^[./]",
   ],
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderTypeScriptVersion: "5.1.3",
+  importOrderTypeScriptVersion: "5.1.5",
 };
